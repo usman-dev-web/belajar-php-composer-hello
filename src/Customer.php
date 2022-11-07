69bfc624cae83b0f2f0415fc83e2059bf7bce499
M. Usman Maulana <um59411@gmail.com>
2022-11-07 21:07:31 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar;
+class Customer{
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name): string{
+        return "Hello $name, My Name is $this->name" . PHP_EOL;
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'programmerzamannow/belajar-php-composer-hello',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programmerzamannow/belajar-php-composer-hello' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
