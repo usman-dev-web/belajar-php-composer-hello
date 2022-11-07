a9a78c01602962fd78cad03e63806201ef1c0831
M. Usman Maulana <um59411@gmail.com>
2022-11-07 21:44:18 +0700

    Add default value

--- src/Customer.php
+++ src/Customer.php
@@ -7,7 +7,7 @@ class Customer{
         
     }
 
-    public function sayHello(string $name): string{
+    public function sayHello(string $name = "guest"): string{
         return "Hello $name, My Name is $this->name" . PHP_EOL;
     }
 }
